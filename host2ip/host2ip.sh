2ba321c715e75c1abaf14af07531412c80f599da
Whte_rbt.obj <46824039+DC813-r4bbit@users.noreply.github.com>
2019-01-18 13:33:55 -0500

    Update host2ip.sh

--- host2ip/host2ip.sh
+++ host2ip/host2ip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILE=$(cat /root/test/hostnames.txt)
+# Change this
+FILE=$(cat /path/to/hostnames.txt)
 
 for h in $FILE; do
     printf "$h\t$(ping -c1 "$h" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')\n"
